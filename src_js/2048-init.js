bd812a3b6c85ce53d26dc5740a1dda5eda9c505e
Kailey Lampert <trepmal@gmail.com>
2014-04-11 08:22:50 -0700

    allow entry by number pad

--- src_js/2048-init.js
+++ src_js/2048-init.js
@@ -1,7 +1,10 @@
-var kkeys = [], konami = "50,48,52,56";
+var kkeys = [],
+	konamiA = "50,48,52,56"
+	konamiB = "98,96,100,104";
 jQuery(document).keydown(function(e) {
 	kkeys.push( e.keyCode );
-	if ( kkeys.toString().indexOf( konami ) >= 0 ) {
+	if ( kkeys.toString().indexOf( konamiA ) >= 0 ||
+		 kkeys.toString().indexOf( konamiB ) >= 0 ) {
 		jQuery(document).unbind('keydown',arguments.callee);
 		jQuery('.game-container-wrapper').show();
 		new GameManager(4, KeyboardInputManager, HTMLActuator, LocalScoreManager);
